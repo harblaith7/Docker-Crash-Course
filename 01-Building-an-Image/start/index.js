e7a7c3b5515baa1bec9790e1be96548347b49001
Laith Harb <35265876+harblaith7@users.noreply.github.com>
2021-01-30 17:56:47 -0500

    completed 01 building image

---
+++ 01-Building-an-Image/start/index.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+const express = require('express')
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    let image = await axios('https://source.unsplash.com/random');
+    res.send(`<img src="${image.request.res.responseUrl}" />`)
+})
+
+
+app.listen((3333), () => {
+    console.log("listening on port 3333!!!!!")
+})
